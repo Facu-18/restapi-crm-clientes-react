eea595ccef654d3376b76b61222152d278db4c03
Facundo <facundomoriconi19@gmail.com>
2024-09-04 10:21:01 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header.js";
 import Navegacion from "./components/layout/Navegacion.js";
 
-// Componentes
+// Componentes clientes
 import Clientes from "./components/clientes/Clientes.js";
 import NuevoCliente from "./components/clientes/NuevoCliente.js";
 import EditarCliente from "./components/clientes/EditarCliente.js";
 
 import Pedidos from "./components/pedidos/Pedidos.js";
+
+// Componentes Productos
 import Productos from "./components/productos/Productos.js";
+import NuevoProducto from "./components/productos/NuevoProducto.js";
+import EditarProductos from "./components/productos/EditarProducto.js";
 
 function App(){
   return(
@@ -31,6 +35,8 @@ function App(){
               <Route exact path="/clientes/editar/:id" Component={EditarCliente}/>
 
               <Route exact path="/productos" Component={Productos} />
+              <Route exact path="/productos/nuevo" Component={NuevoProducto} />
+              <Route exact path="/productos/editar" Component={EditarProductos} />
               
               <Route exact path="/pedidos" Component={Pedidos} />
             </Routes>
