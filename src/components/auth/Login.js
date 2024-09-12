8aa8adf75dbd8d723ee546ce903f3dc2896d1b31
Facundo <facundomoriconi19@gmail.com>
2024-09-12 17:14:17 -0300

    commit

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,4 @@
-import React,{Fragment, useState, useEffect, useContext}from 'react';
+import React, { Fragment, useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import clientesAxios from '../../config/axios.js';
@@ -6,8 +6,8 @@ import clientesAxios from '../../config/axios.js';
 // context
 import { CRMContext } from '../../context/CRMContext.js';
 
-function Login(){
-    
+function Login() {
+
     // auth y token
     const [auth, guardarAuth] = useContext(CRMContext);
 
@@ -21,80 +21,89 @@ function Login(){
         e.preventDefault();
 
         // autenticar al usario
-        try{
+        try {
             const respuesta = await clientesAxios.post('/usuarios/iniciar-sesion', credenciales);
-            
+
             // extraer token y guardarlo en local storage
-            const {token} = respuesta.data;
+            const { token } = respuesta.data;
             localStorage.setItem('token', token);
 
             // colocar en el state
             guardarAuth({
-                token, 
+                token,
                 auth: true
             })
-            
+
             // alerta
             Swal.fire(
                 'Bienvenido',
                 'Has iniciado sesion correctamente',
                 'success'
             )
-            
+
             navigate('/')
-           
-        }catch(error){
-            console.log(error)
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: error.response.data.mensaje
-            })
+
+        } catch (error) {
+
+            if (error.response) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: error.response.data.mensaje
+                })
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Hubo un error'
+                })
+
+            }
         }
     }
 
     // almacenar los datos en el state
-    const leerDatos = (e)=>{
-       guardarCredenciales({
-        ...credenciales,
-        [e.target.name] : e.target.value
-       })
+    const leerDatos = (e) => {
+        guardarCredenciales({
+            ...credenciales,
+            [e.target.name]: e.target.value
+        })
     }
-    
-    return(
+
+    return (
         <Fragment>
             <div className='login'>
                 <h2>Iniciar Sesión</h2>
 
                 <div className='.contenedor-formulario'>
                     <form
-                     onSubmit={iniciarSesion}
+                        onSubmit={iniciarSesion}
                     >
                         <div className='campo'>
                             <label>Email</label>
                             <input
-                              type='text'
-                              name='email'
-                              placeholder='Tu email'
-                              required
-                              onChange={leerDatos}
+                                type='text'
+                                name='email'
+                                placeholder='Tu email'
+                                required
+                                onChange={leerDatos}
                             />
                         </div>
 
                         <div className='campo'>
                             <label>Tu Contraseña</label>
                             <input
-                              type='password'
-                              name='password'
-                              placeholder='Tu Contraseña'
-                              required
-                              onChange={leerDatos}
+                                type='password'
+                                name='password'
+                                placeholder='Tu Contraseña'
+                                required
+                                onChange={leerDatos}
                             />
                         </div>
-                        <input type='submit' value='Iniciar Sesión' className='btn btn-verde btn-block'/>
+                        <input type='submit' value='Iniciar Sesión' className='btn btn-verde btn-block' />
                     </form>
                 </div>
-             </div>
+            </div>
         </Fragment>
     )
 }
